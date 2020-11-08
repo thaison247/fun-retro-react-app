beb115e28f80de06b899877e6f635c4f09ebfcd4
thaison247 <nguyenthaison247@gmail.com>
2020-11-08 15:29:47 +0700

    show alert registered successfully or fail after submitted

--- src/features/Users/pages/RegisterForm/index.js
+++ src/features/Users/pages/RegisterForm/index.js
@@ -1,19 +1,37 @@
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Alert } from "antd";
 import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
 import "./style.css";
 import axios from "axios";
+import { useState } from "react";
 
 const RegisterForm = () => {
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+
   const onFinish = async (values) => {
     console.log("Received values of form: ", values);
-    if (values.password != values.repeat_password) {
-      console.log("Error");
+
+    if (values.repeat_password != values.password) {
+      setAlertType("error");
+      setAlertMessage("Repeated password does not match with password!");
     } else {
-      const res = await axios.post(
-        "http://localhost:3001/users/register",
-        values
-      );
-      console.log("after post req: ", res);
+      try {
+        const res = await axios.post(
+          "http://localhost:3001/users/register",
+          values
+        );
+
+        console.log(res.data);
+        setAlertType("success");
+        setAlertMessage(res.data.message);
+      } catch (err) {
+        console.log(err);
+        setAlertType("error");
+        setAlertMessage(err.message);
+      } finally {
+        setAlertVisible(true);
+      }
     }
   };
 
@@ -92,6 +110,19 @@ const RegisterForm = () => {
         </Button>
         Or <a href="">Login now!</a>
       </Form.Item>
+      {alertVisible && (
+        <Alert
+          message={alertMessage}
+          // description="You have changed your information."
+          type={alertType}
+          closable={true}
+          style={{ width: 500 }}
+          showIcon
+          onClose={() => {
+            setAlertVisible(false);
+          }}
+        />
+      )}
     </Form>
   );
 };
